d25c14e76033b29281471739ea241a8c20d01d9d
Zozoom <kiss.zoltan.pti@gmail.com>
2023-09-07 22:15:05 +0200

    adding actions

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 5000;
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/name', (req, res) => {
   res.send('Hello World!');
