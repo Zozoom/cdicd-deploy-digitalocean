d1403a93181417583ce0eb41eeedd0ccafb63a3c
Zozoom <kiss.zoltan.pti@gmail.com>
2023-09-07 23:00:01 +0200

    little change lets see

--- server/app.js
+++ server/app.js
@@ -3,10 +3,18 @@ const app = express();
 const path = require('path');
 const port = 5000;
 
+// current timestamp in milliseconds
+let ts = Date.now();
+
+let date_ob = new Date(ts);
+let date = date_ob.getDate();
+let month = date_ob.getMonth() + 1;
+let year = date_ob.getFullYear();
+
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/name', (req, res) => {
-  res.send('Hello World! - Changed');
+  res.send(`Works ${year} + "-" + ${month} + "-" + ${date}`);
 });
 
 app.listen(port, () => {
